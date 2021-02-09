8a6a9192d25d0d0bd7528c7ba08e31224381da0f
Gustavo Polar <gpolars@gmail.com>
2021-02-09 00:09:12 -0200

    CLASE 5

--- src/components/ListaNotas.jsx
+++ src/components/ListaNotas.jsx
@@ -1,19 +1,18 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import DetalleNotas from "./DetalleNotas";
 
 class ListaNotas extends Component {
   render() {
     return (
       <ul>
-        <li>
-          <DetalleNotas />
-        </li>
-        <li>
-          <DetalleNotas />
-        </li>
-        <li>
-          <DetalleNotas />
-        </li>
+        {Array.of("React", "Oracle", "Java", "C#").map((curso, indice) => {
+          return (
+            <li key={indice}>
+              <div>{curso}</div>
+              <DetalleNotas/>
+            </li>
+          );
+        })}
       </ul>
     );
   }
