6f9d9ba423e52929e121955556df01f034a8919e
Gustavo Polar <gpolars@gmail.com>
2021-05-09 12:33:15 -0300

    Aula 2_1_1

--- src/components/detalleNotas/DetalleNotas.jsx
+++ src/components/detalleNotas/DetalleNotas.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import "./estilo.css";
+import {ReactComponent as DeleteSVG} from "../../assets/imagens/delete.svg";
 
 class DetalleNotas extends Component {
   render() {
@@ -7,6 +8,7 @@ class DetalleNotas extends Component {
       <section className="detalle-nota">
         <header className="detalle-nota_header">
           <h3 className="detalle-nota_titulo">{this.props.titulo}</h3>
+          <DeleteSVG/>
         </header>
         <p className="detalle-nota_texto">{this.props.nota}</p>
       </section>
